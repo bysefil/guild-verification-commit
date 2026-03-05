cd3cbe6974387652d31741796dc2c37ab9767a90
Sefil <pattisonlara@gmail.com>
2026-03-05 16:54:05 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:53:09.131Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T13:54:03.799Z
+Commit: Fix memory leak in event handlers
