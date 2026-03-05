4905d4f9a2e41991eaebe7b16e619759c3e63a70
Sefil <pattisonlara@gmail.com>
2026-03-06 01:16:54 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:16:47.288Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T22:16:52.724Z
+Commit: Fix broken links in documentation
