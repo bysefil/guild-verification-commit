61597169b4366d21f5128406f6cd90de6d6db9cd
Sefil <pattisonlara@gmail.com>
2026-03-06 01:16:04 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:53:41.854Z
-Commit: Add dark mode support
+Timestamp: 2026-03-05T22:16:02.015Z
+Commit: Resolve null pointer exception in user service
