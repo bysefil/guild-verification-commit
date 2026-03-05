9f56504c83ce7bf03dd5642847b41a60a26cc4a1
Sefil <pattisonlara@gmail.com>
2026-03-05 16:50:32 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:50:29.174Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T13:50:30.378Z
+Commit: Fix CSS styling bug on mobile devices
